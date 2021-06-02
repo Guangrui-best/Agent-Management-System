892e19ea70d7d4b664083333529826d54c6858fb
Guangrui <guangrui@usc.edu>
2021-06-01 23:09:03 -0700

    init

--- users/api/views.py
+++ users/api/views.py
@@ -10,13 +10,14 @@ from rest_framework.decorators import api_view
 from rest_framework import status
 from users.forms import CustomUserForm
 import os
+from django.contrib import messages
 
 
 @api_view(["GET", "POST"])
 def user_list_create_api_view(request):
     
     if request.method == "GET":
-        users = CustomUser.object.all()
+        users = CustomUser.objects.all()
         serializer = UserDisplaySerializer(users, many=True)
         return Response(serializer.data)
 
@@ -24,14 +25,18 @@ def user_list_create_api_view(request):
         serializer = UserDisplaySerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            send_mail(subject="Successful Registration as Agent at Antai Global Inc.",
-                      from_email=os.environ.get('EMAIL_HOST_USER', ''),
-                      recipient_list=[request.email],
-                      fail_silently=True,
-                      html_message="Dear " + str(request.username) + 
+            messages.success(request, "Dear " + str(request.username) + 
                       ",<br><br>Thank you for your registration. Your member id is " + 
                       str(request.id) + 
                       ". Hope you enjoy our web services!<br><br>Best,<br>Antai Global Inc.")
+            # send_mail(subject="Successful Registration as Agent at Antai Global Inc.",
+            #           from_email=os.environ.get('EMAIL_HOST_USER', ''),
+            #           recipient_list=[request.email],
+            #           fail_silently=True,
+            #           html_message="Dear " + str(request.username) + 
+            #           ",<br><br>Thank you for your registration. Your member id is " + 
+            #           str(request.id) + 
+            #           ". Hope you enjoy our web services!<br><br>Best,<br>Antai Global Inc.")
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
